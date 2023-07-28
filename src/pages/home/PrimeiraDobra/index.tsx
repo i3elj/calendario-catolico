3be91958eec8889b488218a6193b0880df155677
i3elj <guisande.bernardo@gmail.com>
2023-07-28 17:30:32 -0300

    identation

--- src/pages/home/PrimeiraDobra/index.tsx
+++ src/pages/home/PrimeiraDobra/index.tsx
@@ -5,25 +5,25 @@ import mockup from './assets/mockup.png'
 import './style.css'
 
 function PrimeiraDobra() {
-    return (
-        <div className="dobra" id="P-dobra">
-            <Logo />
-            <section id="P-dobra__anuncio">
-                <div id="P-dobra__copy">
-                    <h2>INSCRIÇÕES ENCERRADAS</h2>
-                    <p>Que pena, as vendas do kit “O básico que todo Católico precisa saber” foram encerradas. Mas você pode se cadastrar na lista de espera para ser avisado, em primeira mão, quando disponibilizarmos o produto novamente.</p>
-                    <h3>Cadastre seus dados aqui em baixo:</h3>
-                    <div id="P-dobra__form">
-                        <input type="text" name="nome" placeholder="Nome" />
-                        <input type="email" name="email" placeholder="Email" />
-                        <Link to='obrigado'>SIM, EU QUERO ME INSCREVER</Link>
-                    </div>
-                    <span>*Você receberá uma confirmação por e-mail, abra-o e favorite-o para não perder nenhuma novidade.</span>
-                </div>
-                <img id="P-dobra__imagem" src={mockup} />
-            </section>
-        </div >
-    )
+  return (
+    <div className="dobra" id="P-dobra">
+      <Logo />
+      <section id="P-dobra__anuncio">
+        <div id="P-dobra__copy">
+          <h2>INSCRIÇÕES ENCERRADAS</h2>
+          <p>Que pena, as vendas do kit “O básico que todo Católico precisa saber” foram encerradas. Mas você pode se cadastrar na lista de espera para ser avisado, em primeira mão, quando disponibilizarmos o produto novamente.</p>
+          <h3>Cadastre seus dados aqui em baixo:</h3>
+          <div id="P-dobra__form">
+            <input type="text" name="nome" placeholder="Nome" />
+            <input type="email" name="email" placeholder="Email" />
+            <Link to='obrigado'>SIM, EU QUERO ME INSCREVER</Link>
+          </div>
+          <span>*Você receberá uma confirmação por e-mail, abra-o e favorite-o para não perder nenhuma novidade.</span>
+        </div>
+        <img id="P-dobra__imagem" src={mockup} />
+      </section>
+    </div >
+  )
 }
 
 export default PrimeiraDobra
--- src/pages/home/SegundaDobra/index.tsx
+++ src/pages/home/SegundaDobra/index.tsx
@@ -3,23 +3,23 @@ import guia from './assets/Capa - Guia.png'
 import calendario from './assets/Capa - Calendário.png'
 
 export default function SegundaDobra() {
-    return (
-        <div className="dobra" id="S-dobra">
-            <section id="S-dobra__section-1">
-                <h1>PRA QUEM É O BÁSICÃO DO CATÓLICO?</h1>
-                <p>Que pena, as vendas do kit “O básico que todo Católico precisa saber” foram encerradas. Mas você pode se cadastrar na lista de espera para ser avisado, em primeira mão, quando disponibilizarmos o produto novamente.</p>
-            </section>
-            <section id="S-dobra__section-2">
-                <h1>E O QUE EU LEVO AO ADQUIRIR O KIT?</h1>
-                <div>
-                    <img id="calendario" src={calendario} />
-                    <p>Calendário 2023</p>
-                </div>
-                <div>
-                    <img id="guia" src={guia} />
-                    <p>Guia da Missa</p>
-                </div>
-            </section>
+  return (
+    <div className="dobra" id="S-dobra">
+      <section id="S-dobra__section-1">
+        <h1>PRA QUEM É O BÁSICÃO DO CATÓLICO?</h1>
+        <p>Que pena, as vendas do kit “O básico que todo Católico precisa saber” foram encerradas. Mas você pode se cadastrar na lista de espera para ser avisado, em primeira mão, quando disponibilizarmos o produto novamente.</p>
+      </section>
+      <section id="S-dobra__section-2">
+        <h1>E O QUE EU LEVO AO ADQUIRIR O KIT?</h1>
+        <div>
+          <img id="calendario" src={calendario} />
+          <p>Calendário 2023</p>
         </div>
-    )
+        <div>
+          <img id="guia" src={guia} />
+          <p>Guia da Missa</p>
+        </div>
+      </section>
+    </div>
+  )
 }
